e757f43d0cdf601dcec8cc9ae5604348ceeab100
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-05-01 17:37:09 +0200

    add the delete the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import data from "./data/data.json";
 
 const App = () => {
   const { results } = data;
-  const apartements = results.map((apartement) => {
+  const [apartements, setApartements] = useState(results);
+
+  const handleDelete = (id) => {
+    const filtered = apartements.filter((apartement) => apartement.id !== id);
+    setApartements(filtered);
+  };
+  const apartementList = apartements.map((apartement) => {
     return (
-      <>
+      <div key={apartement.id}>
         <p>{apartement.name}</p>
         <p>Price: {apartement.price}</p>
         {apartement.cancellation_policy === "flexible" && <p>flexible</p>}
-      </>
+        <button
+          onClick={() => {
+            handleDelete(apartement.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     );
   });
 
   return (
     <>
       <h1>Appartements List.</h1>
-      <ul>{apartements}</ul>
+      <ul>{apartementList}</ul>
     </>
   );
 };
